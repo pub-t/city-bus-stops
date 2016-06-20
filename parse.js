10a729d2a1b5b830ab9e79f5ecbb43ec78d88048
Дмитрий Прокопович < >
2016-06-20 11:10:47 +0300

    updated data record: new Schema - relation

--- parse.js
+++ parse.js
@@ -1,8 +1,13 @@
 var request = require('request');
-var Bus = require('./bus');
+var model = require('./model');
 var db = require('./save');
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
-var query = 'http://overpass-api.de/api/interpreter?data=[out:json];node[highway=bus_stop][bus=yes](53.61776477868735,23.714332580566406,53.7450563448116,23.971824645996094);out;';
+var Bus = model.Bus;
+var Relation = model.Relation;
+
+var query = 'http://overpass-api.de/api/interpreter?data=[out:json];(node["highway"="bus_stop"]["bus"="yes"](53.61776477868735,23.714332580566406,53.7450563448116,23.971824645996094);<;);out meta;';
 
 var parametr = '[public_transport=platform]'; //мб понадобится
 
@@ -12,22 +17,70 @@ request(query, function (err, res, page) {
        var data = JSON.parse(page);
        var bus_stop = data.elements;
 
-       bus_stop.forEach(function (item, i) {
+       bus_stop.forEach(function (item) {
 
-           var tags = item.tags;
-
-
+          Bus.findOne({id: item.id}, function(error, elem){
+            if(elem!=null){
+              console.log("this ID:  "+elem.id+" exist");
+            }else if(item.type == "node"){
+              var tags = item.tags;
 
                bus = new Bus({
+                   id: item.id,
                    name: tags.name,
                    lat: item.lat,
                    lon: item.lon
+                 });
 
                 db.add(bus);
 
+            }
+
+            });
+
+            Relation.findOne({id: item.id}, function (error, elem) {
+              if(elem!=null){
+                console.log("relation already exist");
+              }else if(item.type=="relation"){
+
+                var tags = item.tags;
+                var members = item.members;
+
+                var mass= [];
+
+                members.forEach(function(elem) {
+                  if(elem.role=="platform"){
+                    mass.push(Math.ceil(elem.ref));
+                  }
+                });
+
+                mass.forEach(function(elem) {
+
+
+                });
+
+                relation = new Relation({
+                id: item.id,
+                from: tags.from,
+                to: tags.to,
+                number: tags.ref,
+                type: tags.route,
+                route:mass
+                });
+
+
+                  db.addRel(relation);
+
+
+
+
+
+              }
+
+            });
+
 
        });
-
 
    }
 });
--- save.js
+++ save.js
@@ -9,4 +9,16 @@ var add = function(bus){
     });
 };
 
+var addRel = function(relation){
+    relation.save(function(error, relation) {
+        if (error){
+            return console.error(error);
+        }
+        else{
+            console.log('Отношение [' + relation.id + '] добавлено.');
+        }
+    });
+};
+
 module.exports.add = add;
+module.exports.addRel = addRel;
