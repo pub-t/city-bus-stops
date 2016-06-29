d33b1dc579dd83f88c6a4c1e1c76fb4f766908d2
tereras <dionp95@mail.ru>
2016-06-29 11:47:27 +0300

    "Change test.js and add more dependencies in package.json"

--- test.js
+++ test.js
@@ -1,18 +1,17 @@
 var request = require('request');
+var expect = require('chai').expect;
 
-var fetchCityBusStops = function (name) {
- var uri = 'http://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area["place"="city"]["name:en"="'+name+'"];(node["highway"="bus_stop"](area););out;';
- request(uri, function (err, res, body) {
-	if(err){
-	 console.error(err);
-	}else {
+var uri = 'http://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area["place"="city"]["name:en"="Hrodna"];(node["highway"="bus_stop"](area););out;';
+describe('check status request on overpass-api (city = Hrodna)', function(){
+ it('should contain node with id 320409795',function(done){
+	this.timeout(15000);
+	request(uri, function(err, res, body){
 	 var data = JSON.parse(body);
-	 var bus_stop = data.elements;
-	 bus_stop.forEach(function (elem) {
-		console.log(elem);
+	 var bus_stops = data.elements;
+	 bus_stops.forEach(function(elem) {
+		expect(elem.id).to.equal(320409795);
+		done();
 	 });
-	}
- });
-};
-
-fetchCityBusStops('Hrodna');
+	});
+ })
+});
