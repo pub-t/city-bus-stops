28198712bd572faf4994e299131e1fabe03695f9
tereras <dionp95@mail.ru>
2016-06-29 12:54:37 +0300

    "Fix fetch all bus stops in osm.js and delete api.js"

--- osm.js
+++ osm.js
@@ -1,8 +1,33 @@
 var request = require('request');
+var osmtogeojson = require('osmtogeojson');
+var fs = require('fs');
+var wstream = fs.createWriteStream('bus_stops.json');
 
 var fetchCityBusStops = function (name) {
  var uri = 'http://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area["place"="city"]["name:en"="'+name+'"];(node["highway"="bus_stop"](area););out;';
- return request(uri);
+ request(uri, function (err, res, doc) {
+  if(err){
+   console.error(err);
+  }else {
+   var data = JSON.parse(doc);
+   var newData = JSON.stringify(osmtogeojson(data));
+   save(newData);
+  }
+ });
+};
+
+var save = function (data) {
+
+ wstream.write(data);
+ wstream.end();
+
+ wstream.on('finish', function() {
+  console.log("well done..");
+ });
+
+ wstream.on('error', function(err){
+  console.log(err.stack);
+ });
 };
 
 module.exports.fetchCityBusStops = fetchCityBusStops;
