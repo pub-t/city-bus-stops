83b45bc4750e42094d1b2553f99f6fed15c336bc
KrivichaninDenis <krivichaninds@gmail.com>
2016-07-08 17:19:10 +0300

    Fix transformToGeoJson function

--- transformNodesToGeoJson.js
+++ transformNodesToGeoJson.js
@@ -1,40 +1,40 @@
 require('buffer').Buffer
-var osmtogeojson = require('osmtogeojson');
-var miss = require('mississippi');
 var util = require('util');
 var Duplex = require('stream').Duplex;
-var buffer = [];
-
 
 function transformNodesToGeoJson(options) {
-  if(!(this instanceof transformNodesToGeoJson)){
+  if (!(this instanceof transformNodesToGeoJson)) {
     return new transformNodesToGeoJson(options)
   }
   Duplex.call(this, options);
+  this.readArr = [];
+  this.flagToRead = true;
 }
 
 util.inherits(transformNodesToGeoJson, Duplex);
 
-transformNodesToGeoJson.prototype._write =  function (chunk, enc, cb) {
-  buffer.push(chunk.toString());
-  console.log('All data: ', buffer);
-  cb();
-};
-
 transformNodesToGeoJson.prototype._read = function readBytes(n) {
   var self = this;
-  console.log('Save data');
-  while (buffer.length) {
-    var chunk = buffer.shift();
+  while (this.readArr.length) {
+    var chunk = this.readArr.shift();
     if (!self.push(chunk)) {
       break;
     }
   }
-  if (buffer.length) {
-    readBytes(self)
+  if (this.flagToRead) {
+    setTimeout(readBytes.bind(self), 1000, n);
   } else {
     self.push(null);
   }
 };
 
-module.exports = new transformNodesToGeoJson();
+transformNodesToGeoJson.prototype._write =
+  function (chunk, enc, cb) {
+    this.readArr.push(new Buffer(chunk));
+    cb();
+  };
+
+var transformObject = new transformNodesToGeoJson();
+
+module.exports = transformObject;
+
